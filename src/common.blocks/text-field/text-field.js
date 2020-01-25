cfec96c3a79cff3465be027eb301fdca6be8972f
Артем Казанцев <akibog@mail.ru>
2020-01-25 09:16:07 +0700

    some tests

--- src/common.blocks/text-field/text-field.js
+++ src/common.blocks/text-field/text-field.js
@@ -2,17 +2,23 @@
 input.onfocus = function(event) {
     let i = event.target;
     i.classList.add('text-field_theme_hover-focused');
+    i.classList.remove('text-field_theme_default');
 }
 
 input.blur = function(event) {
+    let i = event.target;
     i.classList.add('text-field_theme_default');
+    i.classList.remove('text-field_theme_hover-focused');
 }
 
 input.onmouseover = function(event) {
     let i = event.target;
     i.classList.add('text-field_theme_hover-focused');
+    i.classList.remove('text-field_theme_default');
 }
 
 input.onmouseout = function(event) {
+    let i = event.target;
     i.classList.add('text-field_theme_default');
+    i.classList.remove('text-field_theme_hover-focused');
 }
