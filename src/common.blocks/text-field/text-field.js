969dba510456f7e099ba4718e2bd5f12c2b1b24b
Артем Казанцев <akibog@mail.ru>
2020-01-26 09:43:21 +0700

    add focus in CSS delete onfocus from js

--- src/common.blocks/text-field/text-field.js
+++ src/common.blocks/text-field/text-field.js
@@ -1,24 +1,45 @@
+// let input = document.input;
 
-input.onfocus = function(event) {
-    let i = event.target;
-    i.classList.add('text-field_theme_hover-focused');
-    i.classList.remove('text-field_theme_default');
-}
+// input.onfocus = function(event) {
+//     let i = event.target;
+//     i.classList.add('text-field_theme_hover-focused');
+//     i.classList.remove('text-field_theme_default');
+// }
 
-input.blur = function(event) {
-    let i = event.target;
-    i.classList.add('text-field_theme_default');
-    i.classList.remove('text-field_theme_hover-focused');
-}
+// input.blur = function(event) {
+//     let i = event.target;
+//     i.classList.add('text-field_theme_default');
+//     i.classList.remove('text-field_theme_hover-focused');
+// }
 
-input.onmouseover = function(event) {
-    let i = event.target;
-    i.classList.add('text-field_theme_hover-focused');
-    i.classList.remove('text-field_theme_default');
-}
+// input.onmouseover = function(event) {
+//     let i = event.target;
+//     i.classList.add('text-field_theme_hover-focused');
+//     i.classList.remove('text-field_theme_default');
+// }
 
-input.onmouseout = function(event) {
-    let i = event.target;
-    i.classList.add('text-field_theme_default');
-    i.classList.remove('text-field_theme_hover-focused');
-}
+// input.onmouseout = function(event) {
+//     let i = event.target;
+//     i.classList.add('text-field_theme_default');
+//     i.classList.remove('text-field_theme_hover-focused');
+// }
+
+// function text_field_focus (event) {
+//     let i = event.target;
+//     i.classList.add('text-field_theme_hover-focused');
+//     i.classList.remove('text-field_theme_default');
+// }
+
+// function text_field_unfocused (event){
+//     let i = event.target;
+//     i.classList.add('text-field_theme_default');
+//     i.classList.remove('text-field_theme_hover-focused');
+// }
+
+// function addEventList (_id)
+// {   var x = document.getElementById(_id);
+//     x.addEventListener("focus", text_field_focus, true);
+//     x.addEventListener("blur", text_field_unfocused, true);
+//     x.addEventListener("mouseover", text_field_focus, true);
+//     x.addEventListener("mouseout", text_field_unfocused, true);
+// }
