34f1c6e2e5980c4f186e8139d28214977371655a
Артем Казанцев <akibog@mail.ru>
2020-01-31 15:55:32 +0700

    dropdown option button ready

---
+++ src/common.blocks/dropdown-option/dropdown-option.js
@@ -0,0 +1,50 @@
+class DropdownOption {
+    constructor(element){
+        this.option = element;
+        
+        let buttons = this.option.querySelectorAll('.dropdown-option__circle');
+        buttons.forEach(val => {
+            if(val.textContent === '-'){
+                this.minusButton = val;
+            }
+            if (val.textContent === '+'){
+                this.plusButton = val;
+            }
+        });
+
+        this.minusButton.addEventListener('click', this.handleMinusClick.bind(this));
+        this.plusButton.addEventListener('click', this.handlePlusClick.bind(this));
+
+        this.number = this.option.querySelector('.dropdown-option__number');
+        this.value = parseInt(this.number.textContent, 10);
+        if (this.value === '') this.value = 0;
+        if (this.value === 0) this.disableMinus();
+    }
+
+    handleMinusClick(){
+        if(this.value > 0){
+            this.value -= 1;
+        }
+        
+        if(this.value === 0){
+            this.disableMinus();
+        }
+
+        this.number.textContent = this.value;
+    }
+
+    handlePlusClick(){
+        this.value += 1;
+        this.number.textContent = this.value;
+    }
+
+    enableMinus(){
+        this.minusButton.classList.remove('dropdown-option__circle_disable');
+    }
+
+    disableMinus(){
+        this.minusButton.classList.add('dropdown-option__circle_disable');
+    }
+}
+
+export default DropdownOption;
--- src/common.blocks/dropdown/dropdown.js
+++ src/common.blocks/dropdown/dropdown.js
@@ -1,12 +1,32 @@
+import DropdownOption from '../dropdown-option/dropdown-option.js'; 
+
 class DropDown {
     constructor (element){
         this.dropdown = element;
-        this.select = this.dropdown.querySelector('.dropdown__select');
-        this.options = this.dropdown.querySelector('.dropdown__options');
 
+        this.select = this.dropdown.querySelector('.dropdown__select');
         this.select.addEventListener('click', this.handleSelectClick.bind(this));
 
+        this.selectText = this.select.querySelector('.dropdown__select_text');
+        
+        this.options = this.dropdown.querySelector('.dropdown__options');
+        
+        this.optionsList = this.options.querySelectorAll('.dropdown-option'); 
+        this.optionsList.forEach((val => {
+            new DropdownOption(val);
+        }));
+        this.options.addEventListener('DOMSubtreeModified', this.optionsModifed.bind(this));
+        
+        this.optionsText = {
+            guest : {
+                ferst: " гость",
+                second: " гостя",
+                third: " гостей"
+            }            
+        };
     }
+
+    
 
     handleSelectClick(){ 
         if (this.dropdown.classList.contains('dropdown_active')) {
@@ -24,6 +44,45 @@ class DropDown {
     hideDropdown(){
         this.dropdown.classList.remove('dropdown_active');
         this.options.classList.remove('dropdown__options_active');
+    }
+
+    optionsModifed(){
+        this.sum = 0;
+        this.optionsList.forEach((val)=>{
+            let num = val.querySelector('.dropdown-option__number');
+            this.sum += parseInt(num.textContent, 10);
+        });
+        
+        if(this.sum === 0){
+            this.selectText.textContent = 'Сколько гостей';
+            return;
+        }
+        
+        let newval = this.sum % 100;
+        if (newval== 1){
+            this.selectText.textContent = this.sum + this.optionsText.guest.ferst;
+            return;
+        }
+        if(newval > 1 && newval <= 4){
+            this.selectText.textContent = this.sum + this.optionsText.guest.second;
+            return;
+        }
+        if (newval > 4 && newval <= 20){
+            this.selectText.textContent = this.sum + this.optionsText.guest.third;
+            return;
+        }
+        if (newval > 20){
+            let val = this.sum % 10;
+            if (val == 1){
+                this.selectText.textContent = this.sum + this.optionsText.guest.ferst;
+                return;
+            }
+            if(val > 1 && val <= 4){
+                this.selectText.textContent = this.sum + this.optionsText.guest.second;
+                return;
+            }
+        }        
+        this.selectText.textContent = this.sum + this.optionsText.guest.third;
     }
 }
 
