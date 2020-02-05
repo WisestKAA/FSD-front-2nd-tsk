ee43278238435a2c41892a24353e83002c3706d2
Артем Казанцев <akibog@mail.ru>
2020-02-05 13:19:54 +0700

    checkbox ready

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.scss';
-import './common.blocks/text-field/text-field.js'
-import './js/jquery.inputmask'
-import './common.blocks/dropdown/dropdown.js'
+import './common.blocks/text-field/text-field.js';
+import './js/jquery.inputmask';
+import './common.blocks/dropdown/dropdown.js';
+import './common.blocks/checkbox/checkbox.js';
