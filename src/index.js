32c17bec97477621d6c983a4b0fdc1b25f6f285d
Артем Казанцев <akibog@mail.ru>
2020-02-08 21:34:01 +0700

    delete checkbox & add radio-buttons

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import './style.scss';
 import './common.blocks/text-field/text-field.js';
 import './js/jquery.inputmask';
 import './common.blocks/dropdown/dropdown.js';
-import './common.blocks/checkbox/checkbox.js';
 import './common.blocks/expandable-checkbox-list/expandable-checkbox-list.js';
