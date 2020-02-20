988fbde2837fc2f0d4edf01168c1d49e7cfe5ca0
Артем Казанцев <akibog@mail.ru>
2020-02-20 19:14:32 +0700

    work with date-dropdown

---
+++ src/common.blocks/date-dropdown/date-dropdown.js
@@ -0,0 +1,13 @@
+
+class DateDropdown{
+    constructor(elem){
+        this.dateDropdown = elem;
+        this.datePiker = elem.querySelector('.date-dropdown__input');
+        this.datePikerInit(this.datePiker)
+    }
+
+    datePikerInit(datePiker){
+        
+    }
+}
+
