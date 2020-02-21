0a119f1a25c4974f3b3a2a6da61d3545a12f98db
Артем Казанцев <akibog@mail.ru>
2020-02-21 16:04:50 +0700

    date dropdown some add

--- src/common.blocks/date-dropdown/date-dropdown.js
+++ src/common.blocks/date-dropdown/date-dropdown.js
@@ -8,6 +8,35 @@ class DateDropdown{
 
     datePikerInit(datePiker){
         
+        $(datePiker).datepicker({
+            minDate: new Date(),
+            range: true,
+            multipleDatesSeparator: ' - ',
+            dateFormat: 'dd M',
+            language: {
+                days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+                daysShort: ['вос','пон','вто','сре','чет','пят','суб'],
+                daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+                months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+                monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек'],
+                today: 'Сегодня',
+                clear: 'Очистить',
+                dateFormat: 'dd.mm.yyyy',
+                timeFormat: 'hh:ii',
+                firstDay: 1
+            },
+            navTitles: {
+                days: 'MM <i>yyyy</i>'
+            }
+        });
+
+        
     }
 }
 
+$(document).ready(() => {
+    const dateDropdowns = document.querySelectorAll('.date-dropdown');
+    dateDropdowns.forEach((val =>{
+        new DateDropdown(val);      
+    }));
+});
