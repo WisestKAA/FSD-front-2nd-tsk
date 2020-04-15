ca57eccd1ce932dbd56083ae01c22c665ffbf421
Артем Казанцев <akibog@mail.ru>
2020-04-15 11:44:30 +0700

    cards and room-preview ready

--- src/common.blocks/carusel/carusel.js
+++ src/common.blocks/carusel/carusel.js
@@ -4,6 +4,9 @@ import 'owl.carousel/dist/owl.carousel'
 class Carusel{
     constructor(elem){
         this.initCarusel(elem.querySelector('.owl-carousel'));
+        elem.querySelector('.owl-prev').getElementsByTagName('span')[0].textContent = 'keyboard_arrow_left';
+        elem.querySelector('.owl-next').getElementsByTagName('span')[0].textContent = 'keyboard_arrow_right';
+        console.log(this.prev);
     }
 
     initCarusel(owlcarusel){
@@ -11,7 +14,8 @@ class Carusel{
             loop: true,
             items: 1,
             mouseDrag: true,
-            nav: true,            
+            nav: true,   
+            dots: true,         
         });
     }
 }
