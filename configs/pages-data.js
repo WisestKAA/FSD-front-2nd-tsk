4912e9c11c90593a604b9ac3c8d4f7f0280f405b
Артем Казанцев <akibog@mail.ru>
2020-08-20 10:39:15 +0700

    #7 refactored duplicate code in webpack config

---
+++ configs/pages-data.js
@@ -0,0 +1,127 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: './src',
+  uiKit:  './src/pages/ui-kit',
+  wsPages: './src/pages/website-pages',
+}
+
+class PagesData {
+  constructor(){
+    this._data = [
+      {
+        name: 'index',
+        jsPath: `${PATHS.src}/index.js`,
+        stylePath: `${PATHS.src}/style.scss`,
+        template: `${PATHS.src}/index.pug`,
+        filename: './index.html',
+        chunks: ['index']
+      },
+      {
+        name: 'cards',
+        jsPath: `${PATHS.uiKit}/cards/cards.js`,
+        stylePath: `${PATHS.uiKit}/cards/cards.scss`,
+        template: `${PATHS.uiKit}/cards/cards.pug`,
+        filename: './cards.html',
+        chunks: ['cards']
+      },
+      {
+        name: 'colors-and-type',
+        stylePath: `./src/pages/ui-kit/colors-and-type/colors-and-type.scss`,
+        template: `./src/pages/ui-kit/colors-and-type/colors-and-type.pug`,
+        filename: './colors-and-type.html',
+        chunks: ['colors-and-type']
+      },
+      {
+        name: 'form-elements',
+        jsPath: `${PATHS.uiKit}/form-elements/form-elements.js`,
+        stylePath: `${PATHS.uiKit}/form-elements/form-elements.scss`,
+        template: `${PATHS.uiKit}/form-elements/form-elements.pug`,
+        filename: './form-elements.html',
+        chunks: ['form-elements']
+      },
+      {
+        name: 'headers-and-footers',
+        jsPath: `${PATHS.uiKit}/headers-and-footers/headers-and-footers.js`,
+        stylePath: `${PATHS.uiKit}/headers-and-footers/headers-and-footers.scss`,
+        template: `${PATHS.uiKit}/headers-and-footers/headers-and-footers.pug`,
+        filename: './headers-and-footers.html',
+        chunks: ['headers-and-footers']
+      },
+      {
+        name: 'landing',
+        jsPath: `${PATHS.wsPages}/landing/landing.js`,
+        stylePath: `${PATHS.wsPages}/landing/landing.scss`,
+        template: `${PATHS.wsPages}/landing/landing.pug`,
+        filename: './landing.html',
+        chunks: ['landing']
+      },
+      {
+        name: 'registration',
+        jsPath: `${PATHS.wsPages}/registration/registration.js`,
+        stylePath: `${PATHS.wsPages}/registration/registration.scss`,
+        template: `${PATHS.wsPages}/registration/registration.pug`,
+        filename: './registration.html',
+        chunks: ['registration']
+      },
+      {
+        name: 'room-details',
+        jsPath: `${PATHS.wsPages}/room-details/room-details.js`,
+        stylePath: `${PATHS.wsPages}/room-details/room-details.scss`,
+        template: `${PATHS.wsPages}/room-details//room-details.pug`,
+        filename: './room-details.html',
+        chunks: ['room-details']
+      },
+      {
+        name: 'search-room',
+        jsPath: `${PATHS.wsPages}/search-room/search-room.js`,
+        stylePath: `${PATHS.wsPages}/search-room/search-room.scss`,
+        template: `${PATHS.wsPages}/search-room/search-room.pug`,
+        filename: './search-room.html',
+        chunks: ['search-room']
+      },
+      {
+        name: 'sign-in',
+        jsPath: `${PATHS.wsPages}/sign-in/sign-in.js`,
+        stylePath: `${PATHS.wsPages}/sign-in/sign-in.scss`,
+        template: `${PATHS.wsPages}/sign-in/sign-in.pug`,
+        filename: './sign-in.html',
+        chunks: ['sign-in']
+      },  
+    ];
+  }
+
+  get entry (){
+    let result = {};
+    this._data.forEach((val) => {
+      if(!val.jsPath){
+        result[val.name] = val.stylePath;
+      } else if (!val.stylePath) {
+        result[val.name] = val.jsPath;
+      } else {      
+        result[val.name] = [
+          val.jsPath,
+          val.stylePath,
+        ];
+      }
+    });
+    return result;
+  }
+
+  get HTMLPlugins (){
+    let plugins = [];
+    this._data.forEach((val) => {
+      plugins.push(
+        new HtmlWebpackPlugin({
+          template: val.template,
+          filename: val.filename,
+          inject: true,
+          chunks: val.chunks,
+        })
+      )
+    });
+    return plugins;
+  }
+}
+
+module.exports = new PagesData();
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,20 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require ('webpack');
+const pagesData = require ('./configs/pages-data')
 
 module.exports = (env, options) => {
   const PATHS = {
     src: path.join(__dirname, './src'),
     dist: path.join(__dirname, './out'),
   };
-  const PAGES_DIR = `${PATHS.src}/pages/`;
   const production = options.mode === 'production';
   const publicDir = production ? 'https://wisestkaa.github.io/FSD-front-2nd-tsk/' : '/';
   
   
   return{
     devtool: production ? false : 'eval-sourcemap',
-    entry:{
-      'index': [
-        `${PATHS.src}/index.js`,
-        `${PATHS.src}/style.scss`
-      ],
-      'cards': [
-        `${PATHS.src}/pages/ui-kit/cards/cards.js`,
-        `${PATHS.src}/pages/ui-kit/cards/cards.scss`,
-      ],   
-      'colors-and-type': [
-        `${PATHS.src}/pages/ui-kit/colors-and-type/colors-and-type.scss`,
-      ],
-      'form-elements': [
-        `${PATHS.src}/pages/ui-kit/form-elements/form-elements.js`,
-        `${PATHS.src}/pages/ui-kit/form-elements/form-elements.scss`,
-      ],
-      'headers-and-footers': [
-        `${PATHS.src}/pages/ui-kit/headers-and-footers/headers-and-footers.js`,
-        `${PATHS.src}/pages/ui-kit/headers-and-footers/headers-and-footers.scss`,
-      ],
-      'landing': [
-        `${PATHS.src}/pages/website-pages/landing/landing.js`,
-        `${PATHS.src}/pages/website-pages/landing/landing.scss`,
-      ],
-      'registration': [
-        `${PATHS.src}/pages/website-pages/registration/registration.js`,
-        `${PATHS.src}/pages/website-pages/registration/registration.scss`,
-      ],
-      'room-details': [
-        `${PATHS.src}/pages/website-pages/room-details/room-details.js`,
-        `${PATHS.src}/pages/website-pages/room-details/room-details.scss`,
-      ],
-      'search-room': [
-        `${PATHS.src}/pages/website-pages/search-room/search-room.js`,
-        `${PATHS.src}/pages/website-pages/search-room/search-room.scss`,
-      ],
-      'sign-in': [
-        `${PATHS.src}/pages/website-pages/sign-in/sign-in.js`,
-        `${PATHS.src}/pages/website-pages/sign-in/sign-in.scss`,
-      ],
-    },
+    entry: pagesData.entry,    
     output: {
       filename: 'scripts/[name].js',
       path: PATHS.dist,            
@@ -139,12 +98,6 @@ module.exports = (env, options) => {
       contentBase: './out'
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.pug',
-        filename: './index.html',
-        inject: true,
-        chunks: ['index'],
-      }),
       new MiniCssExtractPlugin({
         filename: 'styles/[name].css',
       }),
@@ -152,61 +105,7 @@ module.exports = (env, options) => {
         $: 'jquery',
         jQuery: 'jquery',
         'window.jQuery': 'jquery'
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/ui-kit/colors-and-type/colors-and-type.pug`,
-        filename: './colors-and-type.html',
-        inject: true,
-        chunks: ['colors-and-type'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/ui-kit/form-elements/form-elements.pug`,
-        filename: './form-elements.html',
-        inject: true,
-        chunks: ['form-elements'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/ui-kit/cards/cards.pug`,
-        filename: './cards.html',
-        inject: true,
-        chunks: ['cards'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/ui-kit/headers-and-footers/headers-and-footers.pug`,
-        filename: './headers-and-footers.html',
-        inject: true,
-        chunks: ['headers-and-footers'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/website-pages/landing/landing.pug`,
-        filename: './landing.html',
-        inject: true,
-        chunks: ['landing'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/website-pages/search-room/search-room.pug`,
-        filename: './search-room.html',
-        inject: true,
-        chunks: ['search-room'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/website-pages/room-details/room-details.pug`,
-        filename: './room-details.html',
-        inject: true,
-        chunks: ['room-details'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/website-pages/registration/registration.pug`,
-        filename: './registration.html',
-        inject: true,
-        chunks: ['registration'],
-      }),
-      new HtmlWebpackPlugin({
-        template: `${PAGES_DIR}/website-pages/sign-in/sign-in.pug`,
-        filename: './sign-in.html',
-        inject: true,
-        chunks: ['sign-in'],
-      }),
-    ],
+      }),      
+    ].concat(pagesData.HTMLPlugins),
   };
 }
