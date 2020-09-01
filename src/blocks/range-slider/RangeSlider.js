6a9e0474b9748f3943338f1252a7f6cb420260d6
Артем Казанцев <akibog@mail.ru>
2020-09-01 13:51:33 +0700

    #11 deleted duplicate code in RangeSlider

--- src/blocks/range-slider/RangeSlider.js
+++ src/blocks/range-slider/RangeSlider.js
@@ -2,28 +2,29 @@ import 'ion-rangeslider/js/ion.rangeSlider';
 
 class RangeSlider {
   constructor(elem) {
-    this.$sliderContainer = $(elem);
-    this.$slider = this.$sliderContainer.find('.js-range-slider__slider');
-    this.$price = this.$sliderContainer.find('.js-range-slider__price');
-    this.initSlider();
+    const $sliderContainer = $(elem);
+    const $slider = $sliderContainer.find('.js-range-slider__slider');
+    const $price = $sliderContainer.find('.js-range-slider__price');
+    this._initSlider($slider, $price);
   }
 
-  initSlider() {
-    const { $price } = this;
-    this.$slider.ionRangeSlider({
+  _initSlider($slider, $price) {
+    const that = this;
+    $slider.ionRangeSlider({
       onStart(data) {
-        const { from, to } = data;
-        const fromVal = from.toString().replace(/(\d{1,3})(?=((\d{3})*)$)/g, ' $1');
-        const toVal = to.toString().replace(/(\d{1,3})(?=((\d{3})*)$)/g, ' $1');
-        $price.val(`${fromVal}₽ - ${toVal}₽`);
+        that._changePrice(data, $price);
       },
       onChange(data) {
-        const { from, to } = data;
-        const fromVal = from.toString().replace(/(\d{1,3})(?=((\d{3})*)$)/g, ' $1');
-        const toVal = to.toString().replace(/(\d{1,3})(?=((\d{3})*)$)/g, ' $1');
-        $price.val(`${fromVal}₽ - ${toVal}₽`);
+        that._changePrice(data, $price);
       }
     });
+  }
+
+  _changePrice(data, $price) {
+    const { from, to } = data;
+    const fromVal = from.toString().replace(/(\d{1,3})(?=((\d{3})*)$)/g, ' $1');
+    const toVal = to.toString().replace(/(\d{1,3})(?=((\d{3})*)$)/g, ' $1');
+    $price.val(`${fromVal}₽ - ${toVal}₽`);
   }
 }
 
