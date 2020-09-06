395e58ce8b699942a5cb5329e7bcd09be53b893a
Artem <akibog@mail.ru>
2020-09-06 23:43:48 +0700

    fixed styles for prod

--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -13,7 +13,7 @@ const config = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-                publicPath: './out',
+                publicPath: '../',
             }
           },
           {
