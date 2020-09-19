5cbaec134a230a83c33782b4dbcbb026942ac616
Artem <akibog@mail.ru>
2020-09-19 22:10:31 +0700

    #12 fixed expandable checkbox list

--- src/blocks/expandable-checkbox-list/ExpandableCheckboxList.js
+++ src/blocks/expandable-checkbox-list/ExpandableCheckboxList.js
@@ -10,6 +10,8 @@ class ExpandableCheckboxList {
   _init() {
     this._header = this._elem.querySelector('.js-expandable-checkbox-list__header');
     this._checkboxes = this._elem.querySelector('.js-expandable-checkbox-list__checkboxes');
+    this._arrow = this._elem.querySelector('.js-expandable-checkbox-list__arrow')
+      .querySelector('.js-material-icons');
   }
 
   _addEvents() {
@@ -18,9 +20,13 @@ class ExpandableCheckboxList {
 
   @boundMethod
   _handleEclClick() {
-    this._checkboxes.classList.contains('expandable-checkbox-list__checkboxes_display')
-      ? this._checkboxes.classList.remove('expandable-checkbox-list__checkboxes_display')
-      : this._checkboxes.classList.add('expandable-checkbox-list__checkboxes_display');
+    if (this._checkboxes.classList.contains('expandable-checkbox-list__checkboxes_display')) {
+      this._checkboxes.classList.remove('expandable-checkbox-list__checkboxes_display');
+      this._arrow.textContent = 'keyboard_arrow_down';
+    } else {
+      this._checkboxes.classList.add('expandable-checkbox-list__checkboxes_display');
+      this._arrow.textContent = 'keyboard_arrow_up';
+    }
   }
 }
 
