a2e4d53afb090a4c3f9d7ceef1e7ad7555eedd12
Artem <akibog@mail.ru>
2020-09-19 20:00:27 +0700

    #12 fixed masked text field

--- src/blocks/text-field/TextField-init.js
+++ src/blocks/text-field/TextField-init.js
@@ -1,7 +1,7 @@
 import TextField from './TextField';
 
 $(document).ready(() => {
-  const filteredTextFields = document.querySelectorAll('js-text-field_masked');
+  const filteredTextFields = document.querySelectorAll('.js-text-field_masked');
   filteredTextFields.forEach((elem) => {
     new TextField(elem);
   });
