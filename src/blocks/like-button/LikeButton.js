f083e80ee06de385f154271a0c8368fee47f2f70
Артем Казанцев <akibog@mail.ru>
2021-03-01 15:46:48 +0700

    #18 fixed like button

--- src/blocks/like-button/LikeButton.js
+++ src/blocks/like-button/LikeButton.js
@@ -10,6 +10,8 @@ class LikeButton {
   _init() {
     this._icon = this._likeButton.querySelector('.material-icons');
     this._num = this._likeButton.querySelector('.like-button__number');
+    this._currentValue = parseInt(this._num.textContent, 10);
+    this._num.textContent = this._getCorrectValue(this._currentValue);
   }
 
   _addEvents() {
@@ -18,17 +20,33 @@ class LikeButton {
 
   @boundMethod
   _handleLikeButtonClick() {
-    let num;
     if (this._likeButton.classList.contains('like-button_active')) {
       this._likeButton.classList.remove('like-button_active');
-      num = parseInt(this._num.textContent, 10);
-      this._num.textContent = num - 1;
+      this._num.textContent = this._getCorrectValue(this._currentValue - 1);
       this._icon.textContent = 'favorite_border';
     } else {
       this._likeButton.classList.add('like-button_active');
-      num = parseInt(this._num.textContent, 10);
-      this._num.textContent = num + 1;
+      this._num.textContent = this._getCorrectValue(this._currentValue + 1);
       this._icon.textContent = 'favorite';
+    }
+  }
+
+  _getCorrectValue(value) {
+    let val = Number(value);
+    const strVal = String(value);
+    switch (true) {
+      case val > 1000000000: {
+        return `${strVal.slice(0, strVal.length - 9)}B+`;
+      }
+      case val > 1000000: {
+        return `${strVal.slice(0, strVal.length - 6)}M+`;
+      }
+      case val > 1000: {
+        return `${strVal.slice(0, strVal.length - 3)}K+`;
+      }
+      default: {
+        return val;
+      }
     }
   }
 }
