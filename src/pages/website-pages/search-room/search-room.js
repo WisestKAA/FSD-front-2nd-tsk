6c7d39f99f120739242afe5dcc8b87e0d7dfacbc
Артем Казанцев <akibog@mail.ru>
2021-03-23 16:27:20 +0700

    #22 refactoring footer

--- src/pages/website-pages/search-room/search-room.js
+++ src/pages/website-pages/search-room/search-room.js
@@ -3,6 +3,6 @@ import '../../../blocks/date-dropdown/DateDropdown-init';
 import '../../../blocks/dropdown/Dropdown-init';
 import '../../../blocks/expandable-checkbox-list/ExpandableCheckboxList-init';
 import '../../../blocks/room-preview/room-preview';
-import '../../../blocks/footer-with-copyright/footer-with-copyright';
+import '../../../blocks/footer/footer';
 import '../../../blocks/pagination/Pagination-init';
 import '../website-pages-template';
